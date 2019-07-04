b030a96d3c064c9bce92ae9a02ec0f838168abd0
Joe Dracup <josephdracup@chessdigital.co.uk>
2019-07-04 14:13:40 +0100

    Updated task 1.1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,38 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Head = () =>{
-    const course = 'Half Stack application development'
-    return(
+const Head = (props) => {
+    
+    return (
         <div>
-            <h1>{course}</h1>
+            <h1>{props.course}</h1>
         </div>
     )
 }
 
-const Content = (props) =>{
+const Content = (props) => {
     const part1 = 'Fundamentals of React'
     const part2 = 'Using props to pass data'
     const part3 = 'State of a component'
-    return(
+    return (
         <div>
-            <Part name={part1} number={props.exercises1}/>
-            <Part name={part2} number={props.exercises2}/>
-            <Part name={part3} number={props.exercises3}/>
+            <Part name={part1} number={props.exercises1} />
+            <Part name={part2} number={props.exercises2} />
+            <Part name={part3} number={props.exercises3} />
         </div>
     )
 }
 
-const Part = (props) =>{
-    return(
+const Part = (props) => {
+    return (
         <div>
             <p>{props.name} {props.number}</p>
         </div>
     )
 }
 
-const Footer = (props) =>{
-    return(
+const Footer = (props) => {
+    return (
         <div>
             <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
         </div>
@@ -40,17 +40,17 @@ const Footer = (props) =>{
 }
 
 const App = () => {
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
-
-  return (
-    <div>
-        <Head />
-        <Content exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-        <Footer exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-    </div>
-  )
+    const course = 'Half Stack application development'
+    const exercises1 = 10
+    const exercises2 = 7
+    const exercises3 = 14
+    return (
+        <div>
+            <Head course={course}/>
+            <Content exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+            <Footer exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
